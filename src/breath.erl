fe3f1f66baa9bd1d0ace17de9f8f42ceb71f6b89
Pouriya-Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2019-08-11 05:24:35 +0430

    ref: update

--- src/breath.erl
+++ src/breath.erl
@@ -1,71 +1,40 @@
-%%% ------------------------------------------------------------------------------------------------
-%%% Director is available for use under the following license, commonly known as the 3-clause (or
-%%% "modified") BSD license:
-%%%
-%%% Copyright (c) 2018-2019, Pouriya Jahanbakhsh
-%%% (pouriya.jahanbakhsh@gmail.com)
-%%% All rights reserved.
-%%%
-%%% Redistribution and use in source and binary forms, with or without modification, are permitted
-%%% provided that the following conditions are met:
-%%%
-%%% 1. Redistributions of source code must retain the above copyright notice, this list of
-%%%    conditions and the following disclaimer.
-%%%
-%%% 2. Redistributions in binary form must reproduce the above copyright notice, this list of
-%%%    conditions and the following disclaimer in the documentation and/or other materials provided
-%%%    with the distribution.
-%%%
-%%% 3. Neither the name of the copyright holder nor the names of its contributors may be used to
-%%%    endorse or promote products derived from this software without specific prior written
-%%%    permission.
-%%%
-%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-%%% IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-%%% FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-%%% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-%%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-%%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-%%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-%%% OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-%%% POSSIBILITY OF SUCH DAMAGE.
-%%% ------------------------------------------------------------------------------------------------
-%% @author   Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
-%% @version  18.8.4
-%% @doc
-%%           Set of parse transformation callbacks.
-%% @end
-%% -------------------------------------------------------------------------------------------------
+%%% ----------------------------------------------------------------------------
+%%% @author   <pouriya.jahanbakhsh@gmail.com>
+%%% @version
+%%% @doc
+%%%           Brought a breath of fresh air to the Erlang coding.<br/>
+%%% @end
+
+%% -----------------------------------------------------------------------------
 -module(breath).
 -author("pouriya.jahanbakhsh@gmail.com").
-%% -------------------------------------------------------------------------------------------------
+%% -----------------------------------------------------------------------------
 %% Exports:
 
-%% API:
--export([debug/2]).
-
-%% 'compiler' export:
+%% 'compile' export:
 -export([parse_transform/2]).
 
-%% -------------------------------------------------------------------------------------------------
-%% API:
-
-debug(Mod, AST) ->
-    case os:getenv("ERL_BREATH_DEBUG") of
-        "1" ->
-            io:format("*DBG* ~tp:parse_transform/2 yields:\n~tp\n", [Mod, AST]);
-        _ ->
-            ok
-    end.
-
-%% -------------------------------------------------------------------------------------------------
-%% 'compiler' callback:
+%% -----------------------------------------------------------------------------
+%% 'compile' callback:
 
 %% @hidden
 parse_transform(AST, Opts) ->
+    DebugFlag =
+        case os:getenv("ERL_BREATH_DEBUG") of
+            DebugEnv when DebugEnv == "1" orelse DebugEnv == "true" ->
+                io:format("*DBG* breath: Original AST is:\n~tp\n", [AST]),
+                true;
+            _ ->
+                false
+        end,
     ParseTransformFun =
         fun(Callback, OldAST) ->
-            Callback:parse_transform(OldAST, Opts)
+            NewAST = Callback:parse_transform(OldAST, Opts),
+            _ = DebugFlag andalso io:format(
+                "*DBG*  breath: ~tp:parse_transform/2 yields:\n~tp\n",
+                [Callback, AST]
+            ),
+            NewAST
         end,
     lists:foldl(ParseTransformFun, AST, callbacks()).
 
@@ -73,4 +42,4 @@ parse_transform(AST, Opts) ->
 %% Internals:
 
 callbacks() ->
-    [].
+    [breath_rop, breath_pipeline].
